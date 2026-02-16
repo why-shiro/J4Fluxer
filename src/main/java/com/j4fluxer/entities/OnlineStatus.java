e4bd05428b18a2316dfbbd92261ede1f3c99c1a6
why-shiro <c2126073@student.cankaya.edu.tr>
2026-02-16 22:23:37 +0300

    Remove outdated comments from `OnlineStatus` enum.

--- src/main/java/com/j4fluxer/entities/OnlineStatus.java
+++ src/main/java/com/j4fluxer/entities/OnlineStatus.java
@@ -1,10 +1,10 @@
 package com.j4fluxer.entities;
 
 public enum OnlineStatus {
-    ONLINE("online"),       // Yeşil
-    IDLE("idle"),           // Sarı (Hilal)
-    DND("dnd"),             // Kırmızı (Rahatsız Etmeyin)
-    INVISIBLE("invisible"), // Gri (Görünmez)
+    ONLINE("online"),
+    IDLE("idle"),
+    DND("dnd"),
+    INVISIBLE("invisible"),
     OFFLINE("offline");
 
     private final String key;
